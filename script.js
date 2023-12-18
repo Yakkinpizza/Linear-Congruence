f2884539ae1656f07f14587ba295662bb28646b3
Yakkinpizza <153895078+Yakkinpizza@users.noreply.github.com>
2023-12-18 22:29:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ function gcd(a, b) {
 }
 
 function calculate() {
-    const coefficients = document.getElementById('coefficientsInput').value.split(' ').map(Number);
-    const a = coefficients[0];
-    const b = coefficients[1];
-    const c = coefficients[2];
-    const d = coefficients[3];
+    const coefficientsInput = document.getElementById('coefficientsInput').value;
+
+    // Extract coefficients without spaces
+    const a = parseInt(coefficientsInput.charAt(0));
+    const b = parseInt(coefficientsInput.charAt(1));
+    const c = parseInt(coefficientsInput.charAt(2));
+    const d = parseInt(coefficientsInput.charAt(3));
 
     const modValue = parseInt(document.getElementById('modInput').value);
     const powerValue = parseInt(document.getElementById('powerInput').value);
@@ -23,17 +25,30 @@ function calculate() {
     }
 
     if (!foundError) {
-        let X = ((((a * d) - (b * c) % modValue) ** (modValue - 2)) * ((d % modValue) + (-b % modValue))) % modValue;
-        let Y = ((((a * d) - (b * c) % modValue) ** (modValue - 2)) * ((-c % modValue) + (a % modValue))) % modValue;
+        let X = parseInt(((((a * d) - (b * c)) ** (modValue - 2)) * ((d % modValue) + (-b % modValue))) % modValue);
+        let Y = parseInt(((((a * d) - (b * c)) ** (modValue - 2)) * ((-c % modValue) + (a % modValue))) % modValue);
 
         for (let i = 0; i < Math.log2(powerValue); i++) {
-            const X1 = X * (2 + X * (c * b - a * d));
-            const Y1 = Y - X * (d * a * Y - b * c * Y + c - a);
+            const X1 = parseInt(X * (2 + X * (c * b - a * d)));
+            const Y1 = parseInt(Y - X * (d * a * Y - b * c * Y + c - a));
             X = X1;
             Y = Y1;
         }
 
-        result = `X = ${X} + ((${modValue}^${powerValue}) * m) = ${X} + ${Math.pow(modValue, powerValue)}m\nY = ${Y} + ((${modValue}^${powerValue}) * m) = ${Y} + ${Math.pow(modValue, powerValue)}m`;
+        // Check if the final result of X or Y is more than P^n or less than zero
+        if (X < 0 || X >= Math.pow(modValue, powerValue) || Y < 0 || Y >= Math.pow(modValue, powerValue)) {
+            // Calculate new X and Y based on the specified format
+            const X1 = parseInt(X + Math.pow(modValue, powerValue) / Math.pow(modValue, powerValue) * Math.pow(modValue, powerValue));
+            const Y1 = parseInt(Y + Math.pow(modValue, powerValue) / Math.pow(modValue, powerValue) * Math.pow(modValue, powerValue));
+
+            // Calculate the output based on the specified format
+            const outputX = parseInt(X1 + Math.pow(modValue, powerValue) * Math.ceil(-X1 / Math.pow(modValue, powerValue)));
+            const outputY = parseInt(Y1 + Math.pow(modValue, powerValue) * Math.ceil(-Y1 / Math.pow(modValue, powerValue)));
+
+            result = `Adjusted Result:\nX = ${outputX} + ${Math.pow(modValue, powerValue)}m\nY = ${outputY} + ${Math.pow(modValue, powerValue)}m`;
+        } else {
+            result = `Final Result:\nX = ${X} + ${Math.pow(modValue, powerValue)}m\nY = ${Y} + ${Math.pow(modValue, powerValue)}m`;
+        }
     }
 
     document.getElementById('result').innerText = result;
